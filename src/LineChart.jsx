d50129de2ea468efa42e9c183529e40095f74b72
snowleopardAlexa <alexaslomski@gmail.com>
2022-02-11 18:49:26 -0500

    map over permits and dates

--- src/LineChart.jsx
+++ src/LineChart.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef } from 'react';
-import * as d3 from 'd3';
+import * as d3 from 'd3'
 import './LineChart.css';
 
 const LineChart = () => {
@@ -8,11 +8,38 @@ const LineChart = () => {
 
     // fetch data through API endpoint
     useEffect(() => {
-        fetch('https://data.cityofnewyork.us/resource/tg4x-bg4x-b46p.json')
+        fetch('https://data.cityofnewyork.us/resource/tg4x-b46p.json')
           .then(response => response.json())
           .then(data => {
               // transfrom data
               console.log(data)
+
+              // filter through API endpoints
+              const permits = data.filter(e => {
+                  return e.eventtype === 'Shooting Permit'
+              })
+
+              console.log(permits)
+
+              // get all the dates in an array
+              const dates = [...new Set(permits.map(each => each.enteredon.slice(0,10)))]
+
+              let CountsByDate = []
+
+              dates.map(time => {
+                  let date = time
+                  let count = 0
+
+                  permits.map(each => {
+                      let timestamp = each.enterdon.slice(0,10)
+                      if (timestamp === date) {count += 1}
+                  })
+
+                  const counts = {datae: date, count: count}
+                  CountsByDate.push(counts)
+              })
+
+              console.log(CountsByDate);
           })
     })
 
