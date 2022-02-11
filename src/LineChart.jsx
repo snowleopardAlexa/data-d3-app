4163f9a795ae554d9f282c363145a4bd8792bef8
snowleopardAlexa <alexaslomski@gmail.com>
2022-02-11 18:15:13 -0500

    fetch data from endpoint

--- src/LineChart.jsx
+++ src/LineChart.jsx
@@ -3,9 +3,22 @@ import * as d3 from 'd3';
 import './LineChart.css';
 
 const LineChart = () => {
-    return (
-       <div>
 
+    const d3Chart = useRef()
+
+    // fetch data through API endpoint
+    useEffect(() => {
+        fetch('https://data.cityofnewyork.us/resource/tg4x-bg4x-b46p.json')
+          .then(response => response.json())
+          .then(data => {
+              // transfrom data
+              console.log(data)
+          })
+    })
+
+    return (
+       <div id="d3">
+         <svg ref={d3Chart}></svg>
        </div>
     );
 }
