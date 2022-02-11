9c01e937273cbb51b16eef50c2be2d3bacaf825c
snowleopardAlexa <alexaslomski@gmail.com>
2022-02-11 18:05:20 -0500

    create components and import them

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import LineChart from './LineChart';
 
 function App() {
   return (
     <div className="App">
-       
+       <LineChart />
     </div>
   );
 }
---
+++ src/LineChart.jsx
@@ -0,0 +1,13 @@
+import { useEffect, useRef } from 'react';
+import * as d3 from 'd3';
+import './LineChart.css';
+
+const LineChart = () => {
+    return (
+       <div>
+
+       </div>
+    );
+}
+
+export default LineChart;
